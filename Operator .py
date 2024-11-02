d675f3953d427a79eeadc0a0a64e8323aeadf5cf
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-02 19:47:27 +0530

    Operators

---
+++ Operator .py
@@ -0,0 +1,23 @@
+# Arithemetic operators
+
+a = 2
+b = 10 
+
+print(f"Addition-{a+b}")
+print(f"Subtraction-{a-b}")
+print(f"Multiplication-{a*b}")
+print(f"Division-{a/b}")
+print(f"Floor Division-{a//b}")
+print(f"Moduls-{a%b}")
+print(f"Expo-{a**b}")
+
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+result = a+b*a/b
+print(result)
