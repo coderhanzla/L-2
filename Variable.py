09086871e080f122898b868d5c3236e9cbb49b5c
coderhanzla@gmail.com <coderhanzla@gmail.com>
2024-11-02 19:36:02 +0530

    Variable

---
+++ Variable.py
@@ -0,0 +1,17 @@
+name = "Hanzla"
+age = 11
+id = "Coder"
+idd = True
+
+
+print(type(name))
+print(type(age))
+print(type(id))
+print(type(idd))
+
+# After Type Casting
+
+age = float
+print( age , type ( age ))
+idd = str
+print(type( idd) , idd )
